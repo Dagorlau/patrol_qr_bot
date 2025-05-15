b81b3cd7e7362f505c448dadc76d5e41e44c2051
Dagorlau <Dagorlau@gmail.com>
2025-05-15 12:55:24 +0400

    Обновил обработку геолокации и исправил запуск бота

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-rom telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 from datetime import datetime
 
