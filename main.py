3bed2f0fe00bfe8f6009ade9b13a57cc30d6d446
Dagorlau <Dagorlau@gmail.com>
2025-05-16 16:30:50 +0400

    Обновил обработку геолокации и исправил запуск бота

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 from datetime import datetime
 
-ADMINS = [1008592626,1710339009]
+ADMINS = [1008592626,1710339009,670020154]
 
 checkpoints = {
     'door_001': {'name': 'Гараж'},
@@ -37,7 +37,9 @@ async def location_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     location = update.message.location
     if user.id in user_last_point:
         point_code, point_name = user_last_point.pop(user.id)
-        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        timezone = pytz.timezone("Europe/Saratov")
+        # Саратовский часовой пояс 
+        local_time = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
         message = (
             f"Охранник {user.full_name} прошёл точку: {point_name} ({point_code}) в {time}\n"
             f"Геолокация: https://www.google.com/maps?q={location.latitude},{location.longitude}"
